6d6694f51ea32ee76770461638ea0cd8b4a049d5
Nicolás Rincón Mújica <nicolas.rm540@gmail.com>
2019-11-28 12:37:55 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/* VIDEO 3 */
+
+/* ----------------- CODIGO 1 ----------------- */
+function saludar(nombre) {
+    console.log("Hola " + nombre);
+}
+
+var wolverine = {
+    nombre: "Logan"
+};
+
+saludar(wolverine);
+
+/* imprime Hola Object Object */
