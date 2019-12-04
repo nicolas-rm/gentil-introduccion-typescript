395f6fb51b56d56ebc0e70f7ec61a5fab21bb480
Nicolás Rincón Mújica <nicolas.rm540@gmail.com>
2019-12-03 18:06:52 -0600

    FUNCIONES DE FLECHA

--- flecha.js
+++ flecha.js
@@ -1,3 +1,19 @@
+/* FUNCION NORMAL */
 function suma(a, b) {
     return a + b;
 }
+
+console.log(`Funcion de normal: ${suma(15, 15)}`);
+// console.log(suma(20, 10));
+
+
+
+/* FUNCION DE FLECHA PRIMERA FORMA*/
+// let sumar = (a, b) => a + b;
+
+
+/* FUNCION DE FLECHA SEGUNDA FORMA*/
+let sumar = (a, b) => {
+    return a + b;
+};
+console.log(`Funcion de flecha: ${sumar(15, 45)}`);
